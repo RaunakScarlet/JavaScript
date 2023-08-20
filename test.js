4e07d182f2b53553a593040f931a63133a8bcc93
RaunakScarlet <raunakscarlet@gmail.com>
2023-08-20 22:27:16 +0530

    testing

---
+++ test.js
@@ -0,0 +1,47 @@
+
+const arr = [
+    {
+        id: 1,
+        name: "raunak",
+    },
+    {
+        id: 2,
+        name: "rohan",
+    },
+    {
+        id: 3,
+        name: "happy",
+    },
+    {
+        id: 4,
+        name: "bappy",
+    },
+    {
+        id: 5,
+        name: "rakesh",
+    },
+    {
+        id: 6,
+        name: "shivam",
+    }
+]
+
+ const search = "rau";
+// function find(obj) {
+//     if (  obj.name.toLowerCase().includes(search.toLowerCase()))
+//         return obj.id;
+// }
+
+// function find(obj) {
+//     if (obj.id == 3) {
+//         setNam(nam=>obj.name)
+//         return;
+//     }
+// }
+arr.map(obj => {
+    if (obj.id == 3) {
+        
+        return nam=obj.name;
+    }
+ });
+console.log(nam)
